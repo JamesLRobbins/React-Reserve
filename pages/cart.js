5ca533e5a5771afbf698198dbf75c11c37fc19d0
James Robbins <jameslrobbins86@gmail.com>
2020-07-29 20:10:54 -0500

    Database

--- pages/cart.js
+++ pages/cart.js
@@ -1,17 +1,32 @@
+import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import CartItemList from '../components/Cart/CartItemList';
 import CartSummary from '../components/Cart/CartSummary';
 import { parseCookies } from "nookies";
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
+import cookie from 'js-cookie';
 
 
-function Cart({ products }) {
+function Cart({ products, user }) {
+  const [cartProducts, setCartProducts] = React.useState(products)
+
+  async function handleRemoveFromCart(productId) {
+    const url = `${baseUrl}/api/cart`
+    const token = cookie.get('token')
+    const payload = {
+      params: { productId },
+      headers: { Authorization: token }
+    }
+    const response = await axios.delete(url, payload)
+    setCartProducts(response.data)
+  }
+
   console.log(products)
   return (
     <Segment>
-      <CartItemList />
-      <CartSummary />
+      <CartItemList handleRemoveFromCart={handleRemoveFromCart} user={user} products={cartProducts}/>
+      <CartSummary products={[cartProducts]}/>
     </Segment>
   );
 }
